baa45bd9240aa13d067b1c96d573bd9d58f5927b
Akshay Thakare <akshay.thakare.india@gmail.com>
2022-05-23 19:16:35 +0530

    Create solution.ts

---
+++ trees/mirror-tree/solution.ts
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/invert-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+const queue: TreeNode[] = [];
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+    queue.push(root);
+    while (queue.length != 0) {
+        const node = queue.shift();
+        if (node) {
+            swap(node);
+            queue.push(node.left);
+            queue.push(node.right);
+        }
+    }
+    return root;
+};
+
+function swap(node: TreeNode): void {
+    const tmp = node.left;
+    node.left = node.right;
+    node.right = tmp;
+}
